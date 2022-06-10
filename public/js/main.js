29269b9f2431a62462dbf0bc4f11c5946b796ddc
Kimheang-Developer <simkimheang4@gmail.com>
2022-06-10 08:42:46 +0700

    All Compelete

--- public/js/main.js
+++ public/js/main.js
@@ -7,3 +7,19 @@ function closeNav() {
     document.getElementById('sidebar').style.width = "0";
     document.getElementById('overlay').classList.remove('bg-almost-black', 'opacity-75', 'absolute', 'inset-0');
 }
+
+var dropdown = document.getElementsByClassName('dropdown-btn');
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+    dropdown[i].addEventListener('click', function() {
+        this.classList.toggle('active');
+        var dropdownContent = this.nextElementSibling;
+
+        if (dropdownContent.style.display === "block") {
+            dropdownContent.style.display = "none";
+        } else {
+            dropdownContent.style.display = "block";
+        }
+    })
+}
